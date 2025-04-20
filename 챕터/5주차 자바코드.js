829a565020915de5b6ef17f226ad0e1bb35fcda7
wanseongheo <qrstu1253@naver.com>
2025-04-20 13:38:37 +0900

    Update 5주차 자바코드.js

--- 챕터/5주차 자바코드.js
+++ 챕터/5주차 자바코드.js
@@ -14,6 +14,7 @@ if (subject !== null) {
   }
 }
 
+================================================================================
 
 /*for in 문 사용해서 객체 값 가져오기
 for in 문은 객체의 key 값을 가져온다
@@ -29,3 +30,67 @@ const gitBook = {
 for(key in gitBook) {
   document.write(`${key} : ${gitBook[key]}<br>`);
 }
+
+================================================================================
+  
+/* 함수와 매개변수 선언
+*/
+function calcSum(n) {
+  let sum = 0;
+  for(let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+let number = parseInt(prompt("몇까지 더할까요?"));
+
+console.log(`1부터 ${number}까지 더하면 ${calcSum(number)}입니다.`);
+
+
+
+//콜백함수 : 다른함수의 인자로 사용되는 함수
+
+const bttn = document.querySelector("button");     // 버튼 요소 가져옴
+
+function display() {
+  alert("클릭했습니다.");
+}
+
+bttn.addEventListener("click", display);          // 버튼 클릭하면 display 함수 실행 , 뒤에 괄호를 안붙인다 !!
+
+
+================================================================================
+
+//setInterval() 셋인터벌 함수는 웹종료전까지 계속실행된다.
+//clearInterval() 클리어인터벌 함수는 인터벌 종료 
+let counter = 0;
+
+let timer = setInterval(() => { //timer 시작
+  console.log("안녕하세요?")
+  counter++;      // 안녕하세요? 표시되면 counter++
+
+  if (counter === 5)
+    clearInterval(timer);  //counter 5 되면 timer 종료
+}, 2000);   //2초마다 setinterval 함수실행
+
+
+
+================================================================================
+
+//setTimeout() 셋타임아웃 함수
+//지정한 시간이 흐른후에 괄호안에있는 함수(콜백함수) 실행
+
+setTimeout(() => {
+  console.log("안녕하세요?")
+}, 3000);                     //3초후에 콘솔메시지"안녕하세요"
+
+================================================================================
+
+const JeaMok = document.querySelector("#title");
+const EReum = document.querySelector("#desc p");
+const SaJin = document.querySelector("#profile img");
+
+JeaMok.onclick = () => JeaMok.innerText = "프로필";                 //Jeamok 이 #title 이니까 #title을 누르면 "프로필" 바뀐다.
+EReum.onclick = () => EReum.innerHTML = `이름 : <b>민들레</b>`;     //EReum이 #desc p 니까 도레미(기존)에서 민들레로  바뀜
+SaJin.onclick = () => SaJin.src = "images/pf2.png";
